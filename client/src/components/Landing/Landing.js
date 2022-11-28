6b648a805c2957766b778c66bbd2dc60501fa4e0
Daniel <valdez.daniel77@gmail.com>
2022-11-28 02:10:08 -0300

    r

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import Style from "./Landing.module.css" 
 
 export default function Landing(){
 	return(
-		<div>
-			<h1>Bienvenido</h1>
+		<div className={Style.landing}>
+			<div><h1>Bienvenido</h1></div>
 			<Link to="/Home">
 				<button>Ingresar</button>
 			</Link>
